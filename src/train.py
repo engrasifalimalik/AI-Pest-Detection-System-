a12c4c3c6da963c3cffe979c6a55e76314ce46ec
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-28 10:52:26 +0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,31 @@
+import tensorflow as tf
+from preprocessing import load_data
+import os
 
+train_ds, val_ds = load_data("../data/train")
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(1./255),
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(64, 3, activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')  # adjust classes
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(train_ds, validation_data=val_ds, epochs=10)
+
+os.makedirs("../models", exist_ok=True)
+model.save("../models/pest_model.h5")
+
+print("Model trained and saved")
