19946e785b6884b772b99d03818229fbba93eb31
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-28 10:51:50 +0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,25 @@
+import tensorflow as tf
 
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+def load_data(data_dir):
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=42,
+        image_size=IMG_SIZE,
+        batch_size=BATCH_SIZE
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=42,
+        image_size=IMG_SIZE,
+        batch_size=BATCH_SIZE
+    )
+
+    return train_ds, val_ds
