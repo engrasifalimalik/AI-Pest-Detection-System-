eced81bad52cec7a50c84170aefb5428fc324ced
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-28 11:00:21 +0500

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,15 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
 
+model = tf.keras.models.load_model("../models/pest_model.h5")
+
+img_path = "sample.jpg"
+
+img = image.load_img(img_path, target_size=(224, 224))
+img_array = image.img_to_array(img) / 255.0
+img_array = np.expand_dims(img_array, axis=0)
+
+prediction = model.predict(img_array)
+
+print("Predicted Class:", prediction.argmax())
