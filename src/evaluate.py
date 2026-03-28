ce9c2371a51f6fdef302dd2146b63248250b68ef
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-28 10:53:16 +0500

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,10 @@
+import tensorflow as tf
+from preprocessing import load_data
 
+_, val_ds = load_data("../data/train")
+
+model = tf.keras.models.load_model("../models/pest_model.h5")
+
+loss, accuracy = model.evaluate(val_ds)
+
+print("Validation Accuracy:", accuracy)
