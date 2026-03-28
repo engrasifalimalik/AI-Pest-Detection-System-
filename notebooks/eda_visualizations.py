147be684d7b393413c375bc9252c7af763c4a6b3
engrasifalimalik <135517382+engrasifalimalik@users.noreply.github.com>
2026-03-28 11:21:08 +0500

    Update eda_visualizations.py

--- notebooks/eda_visualizations.py
+++ notebooks/eda_visualizations.py
@@ -4,3 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import seaborn as sns
+
+
+# Set dataset path
+DATASET_PATH = "../data/train"  # adjust if needed
+classes = os.listdir(DATASET_PATH)
+print("Classes:", classes)
+print("Number of classes:", len(classes))
+
